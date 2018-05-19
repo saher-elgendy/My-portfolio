9aea6fd918bbdfea009f7351c1659190179e662a
unknown <saherelgendy1990@gmail.com>
2018-05-19 17:14:52 +0300

    final refactoring

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+
+const about_but = document.getElementById("top_button");
+const potfolio_but = document.getElementById("bottom_button");
+
+
+
+
+const scrollToAbout = () =>{
+
+ window.scroll({
+   top: 1350, 
+   left: 0, 
+   behavior: 'smooth'
+ });
+
+
+   
+     
+}
+
+
+const scrollToPortfolio = () =>{
+
+  window.scroll({
+
+   top:1550,
+   left:0,
+   behavior:'smooth'
+
+
+
+  });
+  
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+	
+  
+
