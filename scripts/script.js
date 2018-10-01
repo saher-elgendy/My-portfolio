12c99cb36faeb68d9efd567a8c1b94d3f53985de
unknown <saherelgendy1990@gmail.com>
2018-10-01 20:50:36 +0200

    adding menu links

--- scripts/script.js
+++ scripts/script.js
@@ -2,42 +2,22 @@
 const about_but = document.getElementById("top_button");
 const potfolio_but = document.getElementById("bottom_button");
 
-
-
-
 const scrollToAbout = () =>{
-
  window.scroll({
-   top: 1350, 
+   top: 900, 
    left: 0, 
    behavior: 'smooth'
- });
-
-
-   
-     
+ });   
 }
 
 
 const scrollToPortfolio = () =>{
-
   window.scroll({
-
-   top:1550,
+   top:1180,
    left:0,
    behavior:'smooth'
-
-
-
   });
   
-
-
-
-
-
-
-
 }
 
 
